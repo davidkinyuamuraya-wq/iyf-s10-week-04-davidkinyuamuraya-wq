894cbdc467ba53e7684a62a94da42b0e576369f8
muraya kinyua <davidkinyuamuraya@gmail.com>
2026-03-17 09:55:46 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+// variable declarations
+let name="your name";
+let age=25;
+const birthyear=1999;
+// type of operator
+console.log(typeof name); // string
+console.log(typeof age); //number
+console.log(typeof true); // booleon
+// let vs const
+let score = 100
+score = 150; //works!
+
+const PI = 3.14159;
+// PI = 3; // Error! const cannot be reassigned
+
+// Basic math
+let a = 10;
+let b = 3;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subraction
+console.log( a * b); // Multiplaction
+console.log(a / b); //  Division
+console.log(a % b); // Modulus(remainder)
+console.log(a ** b); //Exponentiation
